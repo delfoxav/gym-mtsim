e12b48b6fec420d03fef8711ebdd65a0be217eb2
delfoxav <delfoxav.students.zhaw.ch>
2025-01-12 11:05:49 +0100

    update

--- gym_mtsim/__init__.py
+++ gym_mtsim/__init__.py
@@ -1,11 +1,11 @@
 
-"""
 from gymnasium.envs.registration import register
 
 from .metatrader import Timeframe, SymbolInfo
 from .simulator import MtSimulator, OrderType, Order, SymbolNotFound, OrderNotFound
 from .envs import MtEnv
 from .data import FOREX_DATA_PATH, STOCKS_DATA_PATH, CRYPTO_DATA_PATH, MIXED_DATA_PATH
+"""
 
 register(
     id='forex-hedge-v0',
